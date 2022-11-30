3e1a0ce434ad43a421caa4c54bed5c0007c8edc7
Monali Sharma <mona.chaudhari444@gmail.com>
2022-11-30 18:22:31 +0530

    mc complete

---
+++ Music_Application/UserTrackService/src/main/java/com/example/challenge/UserTrackService/UserTrackServiceApplication.java
@@ -0,0 +1,28 @@
+package com.example.challenge.UserTrackService;
+
+import com.example.challenge.UserTrackService.filter.JwtFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class UserTrackServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(UserTrackServiceApplication.class, args);
+	}
+	@Bean
+	public FilterRegistrationBean filterBeand(){
+		FilterRegistrationBean filterreg = new FilterRegistrationBean();
+		filterreg.setFilter(new JwtFilter());
+		filterreg.addUrlPatterns("/usertrack/app/addtrack/*");
+		return filterreg;
+	}
+
+}
